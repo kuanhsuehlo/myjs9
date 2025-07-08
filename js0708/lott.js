90a38904b503e75e2f2e88f60d72e2464000ce21
kuanhsuehlo <woody40408@gmail.com>
2025-07-08 16:50:29 +0800

    update-0708

---
+++ js0708/lott.js
@@ -0,0 +1,13 @@
+//排序前
+let sortBefore = document.getElementById("sortBefore");
+// 排序後
+let sortAfter = document.getElementById("sortAfter");
+// 第二區號碼
+let secNo = document.getElementById("secNo");
+
+
+
+// 威力彩
+function lott1() {
+    
+}
